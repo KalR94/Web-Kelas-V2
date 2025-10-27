02877a504ddd999f17f6cb72f822b445689021d4
Kal Stars <haikalramadhan0994@gmail.com>
2025-10-27 18:15:57 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 
 // Ganti dengan URL dan anon key milikmu dari Supabase > Project Settings > API
 const supabaseUrl = "https://dpshbqbrivvdqkubfufk.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwc2hicWJyaXZ2ZHFrdWJmdWZrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTU0Nzg4MSwiZXhwIjoyMDc3MTIzODgxfQ.ZoMdwNW7opkFpSUzHxBq50wCwX889NVbS-EsY6a_5yg";
-
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwc2hicWJyaXZ2ZHFrdWJmdWZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NDc4ODEsImV4cCI6MjA3NzEyMzg4MX0.Xd4zp6EXr5xg1qOkzJPR73rlPnqZEbvVU4x2WcGwnpA";
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
